25c29d095804bcb1f53b78def5cd0b6157ccd766
flph2 <felipe.nix@gmail.com>
2020-03-09 20:07:57 -0300

    Remove pyc from file

--- api/api/settings.py
+++ api/api/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100000
 }
 
 # Database
