ca0ae8fbec22d129e46947dbeab0349d83f61bed
flph2 <felipe.nix@gmail.com>
2020-03-09 09:54:41 -0300

    Fix api startup

--- api/invest/admin.py
+++ api/invest/admin.py
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.contrib import admin
-from django.apps import apps
-
-models = apps.get_models()
-
-for model in models:
-    admin.site.register(model)
--- scripts/setup.py
+++ scripts/setup.py
@@ -3,6 +3,4 @@ dataset1 = pd.read_csv('data/Dataset-1.csv')
 dataset2 = pd.read_csv('data/Dataset-2.csv')
 dataset3 = pd.read_csv('data/Dataset-3.csv')
 
-print(dataset1.columns)
-print(dataset2.columns)
-print(dataset3.columns)
+
