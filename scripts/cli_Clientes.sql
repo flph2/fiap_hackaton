2526d89849497038a21733cf22b9f162d45f2b64
Eduardo Lima <eduardo.analista@outlook.com>
2020-03-21 14:21:35 -0300

    clientes distintos

---
+++ scripts/cli_Clientes.sql
@@ -0,0 +1,62 @@
+
+
+
+
+CREATE TABLE `cli_Clientes` 
+(
+  `userid` 					int 	NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `userid_aux`				text    DEFAULT NULL,
+  `idade` 					double 	DEFAULT NULL,
+  `genero` 					text,
+  `estadocivil` 			text,
+  `billingcity` 			text,
+  `profissaopessoa` 		text,
+  `patrimonioautomoveis` 	double DEFAULT NULL,
+  `patrimonioimoveis` 		double DEFAULT NULL,
+  `patrimonioinvestimentos` double DEFAULT NULL,
+  `patrimoniooutros` 		double DEFAULT NULL,  
+  `nivelconhecimentoatual` 	double DEFAULT NULL,
+  `scorenivelconhecimento` 	double DEFAULT NULL,
+  `perfilinvestidor` 		bigint(20) DEFAULT NULL,
+  `rendamensal` 			double DEFAULT NULL,
+  `valorpatrimonio` 		double DEFAULT NULL,  
+  `scorerisco` 				double DEFAULT NULL,
+  `scoreobjetivos` 			double DEFAULT NULL,
+  `scoresituacaofinanceira` double DEFAULT NULL,
+  `createddate` 			text,
+  `dataclienteefetivado` 	text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+
+CREATE INDEX IDX_CLI ON cli_Clientes(userid)
+
+
+
+insert into cli_Clientes 
+(	userid_aux,idade,genero,estadocivil,billingcity,profissaopessoa,patrimonioautomoveis,patrimonioimoveis,patrimonioinvestimentos,
+	patrimoniooutros,nivelconhecimentoatual,scorenivelconhecimento,perfilinvestidor,rendamensal,valorpatrimonio,
+	scorerisco,scoreobjetivos,scoresituacaofinanceira,createddate,dataclienteefetivado
+)
+SELECT 
+DISTINCT	
+	userid,
+	idade,
+	genero,
+	estadocivil,
+	billingcity,
+	profissaopessoa,
+	patrimonioautomoveis,
+	patrimonioimoveis,
+	patrimonioinvestimentos,
+	patrimoniooutros,	
+	nivelconhecimentoatual,
+	scorenivelconhecimento,
+	perfilinvestidor,
+	rendamensal,
+	valorpatrimonio,
+	scorerisco,
+	scoreobjetivos,
+	scoresituacaofinanceira,
+	createddate,
+	dataclienteefetivado
+from Clientesv2;
