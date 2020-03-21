66190120e25deafa8568236b238fb88fe885b31c
flph2 <felipe.nix@gmail.com>
2020-03-21 15:58:00 -0300

    New table

--- scripts/populate.py
+++ scripts/populate.py
@@ -173,3 +173,46 @@ inner join cli_Clientes c
 conn.execute(q)
 print('cli_respostas data load')
 # Fim Respostas
+
+# Inicio Produtos
+q = '''
+CREATE TABLE cli_Produtos
+(
+        `userid`                        int,
+        `produtoid`             int,
+        `valorrendimento`       double,
+        `dataefetivacao`    text
+);
+'''
+conn.execute(q)
+print('tabela cli_produto criada')
+
+q = '''
+INSERT INTO cli_Produtos (USERID, PRODUTOID, VALORRENDIMENTO, DATAEFETIVACAO)
+SELECT
+DISTINCT
+        c.userid,
+        P.dataId as produtoid,
+        v2.valorrendimento,
+        v2.dataefetivacao
+from Clientesv2 v2
+
+inner join cli_Clientes c
+                on v2.userid = c.userid_aux
+
+inner join Produtos P
+                on P.ProdutoId = v2.produto
+
+UNION ALL
+select
+        distinct
+        0,
+        p.dataid,
+        0,
+        ''
+from Produtos p
+where not exists (select 1 from Clientesv2 v2 where v2.produto = p.ProdutoId)
+'''
+conn.execute(q)
+print('cli_produto data load')
+# Fim cli_Produto
